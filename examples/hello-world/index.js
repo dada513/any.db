959a32ac7483f2bc77b0a160d88956e7a1b9cd9c
dada513 <kontakt@dada513.eu>
2020-06-12 18:07:32 +0200

    run standard --fix

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,22 +1,22 @@
 // require the module
-const any = require("any.db");
+const any = require('any.db')
 
-async function main() {
+async function main () {
   // create the database
-  const db = await any.createDatabase("sqlite", { file: "db.sqlite" });
+  const db = await any.createDatabase('sqlite', { file: 'db.sqlite' })
   // set values in the database
-  await db.set("user1", { email: "someuser@example.com", password: "12345" });
+  await db.set('user1', { email: 'someuser@example.com', password: '12345' })
   // get values
-  await db.get("user1"); // Returns { email: "someuser@example.com", password: "12345" }
+  await db.get('user1') // Returns { email: "someuser@example.com", password: "12345" }
   // get all values
-  /* 
+  /*
       This returns a Map of key => value
       Learn more at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
     */
-  await db.all();
+  await db.all()
   // delete a user
-  await db.delete("user1");
+  await db.delete('user1')
   // check if a key exists in the database
-  await db.has("user1");
+  await db.has('user1')
 }
-main();
+main()
